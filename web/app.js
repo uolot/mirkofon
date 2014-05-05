6c9b456d6676142e352c69f0c949570c96349ba8
Tomasz Walotek <tomasz.walotek@gmail.com>
2014-05-05 21:17:06 +0200

    Add divshot, separate js, add spinner

---
+++ web/app.js
@@ -0,0 +1,70 @@
+var dataUrl = 'http://jsonp.jit.su/?url=http%3A%2F%2Ffreeshell.de%2F~walot%2Fweb%2Fdata.json';
+
+
+Date.prototype.yyyymmdd = function() {
+   var yyyy = this.getFullYear().toString();
+   var mm = (this.getMonth()+1).toString(); // getMonth() is zero-based
+   var dd  = this.getDate().toString();
+   return yyyy + (mm[1]?mm:"0"+mm[0]) + (dd[1]?dd:"0"+dd[0]); // padding
+};
+
+
+var createSpinner = function (targetId) {
+    var opts = {
+      lines: 9, // The number of lines to draw
+      length: 20, // The length of each line
+      width: 10, // The line thickness
+      radius: 30, // The radius of the inner circle
+      corners: 1, // Corner roundness (0..1)
+      rotate: 0, // The rotation offset
+      direction: 1, // 1: clockwise, -1: counterclockwise
+      color: '#fff', // #rgb or #rrggbb or array of colors
+      speed: 1.5, // Rounds per second
+      trail: 60, // Afterglow percentage
+      shadow: false, // Whether to render a shadow
+      hwaccel: false, // Whether to use hardware acceleration
+      className: 'spinner', // The CSS class to assign to the spinner
+      zIndex: 2e9, // The z-index (defaults to 2000000000)
+      top: '50%', // Top position relative to parent
+      left: '50%' // Left position relative to parent
+    };
+
+    var spinTarget = document.getElementById(targetId);
+    return new Spinner(opts).spin(spinTarget);
+}
+
+
+var createTagsList = function (dataUrl) {
+    $.getJSON(dataUrl)
+    .success(function (response) {
+        var source = $('#template').html();
+        var template = Handlebars.compile(source);
+        var context = {
+            time: response.meta.time,
+            links: response.items
+        }
+        var html = template(context);
+
+        $('#content').html(html);
+    })
+}
+
+
+var createEvents = function () {
+    $('#content').delegate('[data-tag]', 'click', function (e) {
+        var tag = $(e.target).data('tag');
+        var tagRef = new Firebase('https://vivid-fire-4385.firebaseio.com/tags/' + tag);
+        tagRef.transaction(function (current) { return current + 1; })
+
+        var ymd = (new Date()).yyyymmdd();
+        var countRef = new Firebase('https://vivid-fire-4385.firebaseio.com/counters/' + ymd);
+        countRef.transaction(function (current) { return current + 1; })
+    });
+}
+
+
+var init = function() {
+    createSpinner('content');
+    createTagsList(dataUrl);
+    createEvents();
+}
